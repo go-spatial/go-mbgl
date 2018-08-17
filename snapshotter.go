b61f0b37ad0fa78c86320749b2a83c558f836cc0
Julio <julio.grillo98@gmail.com>
2018-08-17 12:36:04 -0700

    docstrings

--- snapshotter.go
+++ snapshotter.go
@@ -10,39 +10,51 @@ import (
 	"github.com/go-spatial/geom/slippy"
 )
 
+// This will set the size of the mbgl::ThreadPool
+var DefaultThreadPoolSize = 4
+
+// Snapshotter is an interface for processing raster image from
+// vector tiles.
 type Snapshotter interface {
+	SetStyle(style string)
 	Snapshot(extent *geom.Extent, size image.Point) image.Image
 }
 
+// this is an internal representation o
 type snapshotter struct {
-	fsrc mbgl.FileSource
+	fsrc mbgl.FileSource // an interface
 	snap *mbgl.MapSnapshotter
 	size mbgl.Size
 
+	// the mbgl::MapSnapshotter.Snapshot method is not thread safe
 	snapLock sync.Mutex
 }
 
-var defaultSnapshotter = NewSnapshotter("").(*snapshotter)
-var defaultSnapshotterLock = sync.Mutex{}
-
-func Snapshot(src string, ext *geom.Extent, size image.Point) image.Image {
-	defaultSnapshotterLock.Lock()
-	defer defaultSnapshotterLock.Unlock()
-
-	defaultSnapshotter.snap.SetStyleURL(src)
-	return defaultSnapshotter.Snapshot(ext, size)
+func (s snapshotter) destruct() {
+	s.fsrc.Destruct()
+	s.snap.Destruct()
+	s.size.Destruct()
 }
 
+
+// High level function for taking a single snappshot. This simply creates a new snapshotter and uses it.
+func Snapshot(style string, ext *geom.Extent, size image.Point) image.Image {
+	return NewSnapshotter(style).Snapshot(ext, size)
+}
+
+// This creates an instance of a Snapshotter with the specified style.
+// Note: this high level implementation is thread safe, but performance might be better to lower the DefaultThreadPoolSize and use multiple snapshotters
+// TODO(@ear7h): write benchmarks
 func NewSnapshotter(style string) Snapshotter {
 	src := mbgl.NewDefaultFileSource("", "", nil)
 	runtime.SetFinalizer(src, (*mbgl.DefaultFileSource).Destruct)
 
-	tpool := mbgl.NewThreadPool(4)
+	tpool := mbgl.NewThreadPool(DefaultThreadPoolSize)
 	mbgl.SchedulerSetCurrent(tpool)
 
 	size := mbgl.Size{Width: 100, Height: 100}
 
-	ret := mbgl.NewMapSnapshotter(src,
+	snap := mbgl.NewMapSnapshotter(src,
 		tpool,
 		style,
 		size,
@@ -51,39 +63,54 @@ func NewSnapshotter(style string) Snapshotter {
 		nil,
 		nil)
 
-	runtime.SetFinalizer(ret, (*mbgl.MapSnapshotter).Destruct)
-
-	return &snapshotter{
+	ret := &snapshotter{
 		fsrc: src,
-		snap: ret,
+		snap: snap,
 		size: size,
 	}
+
+	runtime.SetFinalizer(ret, (*snapshotter).destruct)
+
+	return ret
 }
 
+// Take the snapshot with a *geom.Extent encoded as lat/lng (ie. WSG84).
 func (s *snapshotter) Snapshot(extent *geom.Extent, size image.Point) image.Image {
-	s.snapLock.Lock()
-	defer s.snapLock.Unlock()
-
 	a := mbgl.NewLatLng(extent[1], extent[0])
 	b := mbgl.NewLatLng(extent[3], extent[2])
 
-	s.snap.SetRegion(mbgl.NewLatLngBounds(a, b))
+	// the mbgl::MapSnapshotter.Snapshot method is not thread safe
+	// also, we are making lat/lng changes to the class which cannot be
+	// changed through the lifetime of the snapshot routine
+	s.snapLock.Lock()
 
-	a.Destruct()
-	b.Destruct()
+	s.snap.SetRegion(mbgl.NewLatLngBounds(a, b))
 
 	s.size.Width = uint32(size.X)
 	s.size.Height = uint32(size.Y)
 	s.snap.SetSize(s.size)
 
 	_img := s.snap.Snapshot()
+
+	s.snapLock.Unlock()
+
 	img := _img.Image()
-	_img.Destruct()
+
+	go func() {
+		a.Destruct()
+		b.Destruct()
+		_img.Destruct()
+	}()
 
 	return img
 }
 
-func (s *snapshotter) SnapshotTile(tile slippy.Tile, size image.Point) image.Image {
+func (s *snapshotter) SetStyle(style string) {
+	s.snap.SetStyleURL(style)
+}
+
+func SnapshotTile(s Snapshotter, tile slippy.Tile, size image.Point) image.Image {
 	ext := tile.Extent(geom.WGS84)
 	return s.Snapshot(ext, size)
 }
+
